8660fae8b60a76f6af418ec979f1f18271aef555
Brian Henry <brian@>
2018-04-03 16:37:51 -0700

    Add Initial Files

---
+++ calculator/js/scripts.js
@@ -0,0 +1,13 @@
+// USER INTERFACE
+$(document).ready(function() {
+  $("form#newAccount").submit(function(event) {
+    event.preventDefault();
+    var name = $("input#fullName").val();
+    console.log(name):
+    var user = $("input#userName").val();
+    console.log(user):
+    var deposit = parseint$("input#balance");
+    console.log(deposit):
+
+
+})
